a1835003551939f6a93de419e90b9d05cf74a28d
Marce1298 <marchebordon@gmail.com>
2020-09-24 00:40:43 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CondicionalSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Elige medio de transporte(coche, tren, avion)");
+
+            string medioTransporte = Console.ReadLine();
+
+            switch (medioTransporte)
+            {
+
+                case "coche":
+                    Console.WriteLine("Velocidad media: 100km/h");
+                    break;
+
+                case "tren":
+                    Console.WriteLine("Velocidad media: 250km/h");
+                    break;
+
+                case "avion":
+                    Console.WriteLine("Velocidad media: 800km/h");
+                    break;
+
+                default:
+                    Console.WriteLine("Transporte no encontrado");
+                    break;
+
+            }
+            Console.WriteLine("A terminado el programa");
+        }
+    }
+}
